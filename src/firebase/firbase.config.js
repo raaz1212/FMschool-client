6bb8749f4e81e92fd14740955ac2b0f774ddc2c1
raaz1212 <araj30728@gmail.com>
2023-06-10 12:19:38 +0600

    firbase fix

---
+++ src/firebase/firbase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWzCBws7HKoREV_7R1c8XhF73X0TKawo4",
+  authDomain: "radio-jockey-5e76d.firebaseapp.com",
+  projectId: "radio-jockey-5e76d",
+  storageBucket: "radio-jockey-5e76d.appspot.com",
+  messagingSenderId: "615154851297",
+  appId: "1:615154851297:web:646fcfc546f3744d5246a0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
